8e26c219cf4c6305648880eb86dfb3ade7054953
Abhimanyu Tiwary <abhimanyukumartiwary946139@gmail.com>
2024-10-05 17:43:17 +0530

    first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,22 @@
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Hello World");
+        System.out.println("10x1=10");
+		System.out.println("10x2=20");
+		System.out.println("10x3=30");
+		System.out.println("10x4=40");
+		System.out.println("10x5=50");
+		System.out.println("10x6=60");
+		System.out.println("10x7=70");
+		System.out.println("10x8=80");
+		System.out.println("10x9=90");
+		System.out.println("10x10=100");
+		System.out.println(5+10);
+		System.out.println(10-5);
+		System.out.println(10/2);
+		System.out.println(5*2);
+	}
+
+}
